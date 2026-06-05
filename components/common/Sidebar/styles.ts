656ea57d42facf9b380c73c46f4602e4bdfcbe2c
Arvind <your-github-email@example.com>
2026-06-05 12:47:22 +0530

    fixed token name and side bar spacing problem

--- components/common/Sidebar/styles.ts
+++ components/common/Sidebar/styles.ts
@@ -8,10 +8,10 @@ import type { SidebarProps } from './types'
 export const SidebarShell = styled(Layout.Sider)<SidebarProps>`
 	display: flex;
 	position: fixed;
-	top: 0;
+	top: var(--navbar-height);
 	left: 0;
 	z-index: 30;
-	height: 100vh;
+	height: calc(100vh - var(--navbar-height));
 	overflow-y: auto;
 	width: ${({ $collapsed }) => ($collapsed ? '92px' : '288px')};
 	flex-shrink: 0;
